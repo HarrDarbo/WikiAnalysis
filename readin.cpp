d4425e2f4a58abb2e841b09c27cc9e296dbc0f0e
HarrDarbo <hmbd11@gmail.com>
2019-08-13 02:39:56 -0400

    HScore & file systems working

--- readin.cpp
+++ readin.cpp
@@ -11,20 +11,25 @@
 #include <dirent.h>
 #include <unordered_map>
 #include <unistd.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
 
 typedef std::unordered_map<std::string, int> hlist; //structure for hscores
-typedef std::unordered_map<std::string, bool> inlist; //structure for finished hscores and redirects
+typedef std::unordered_map<std::string, bool> inlist; //structure for finished hscores
+typedef std::unordered_map<std::string, std::string> relist; //structure for redirects
 
 //global because its small and convenient
 int writecount = 0;
 std::string type;
+int deep = 0;
 
 //computation methods
 void readarticle(std::string name, std::string &article, std::ofstream &namelist, inlist &dirs); //make text files from XML
 void readarticleL(std::string name, std::string &article, std::ofstream &namelist, inlist &dirs); //make text, but feed to linkArticlesR
 void linkarticles(std::string filename, std::string dirname, std::string directory); //make web files from existing text files
 void linkarticlesR(std::string filename, std::string dirname, std::string &article); //makes web files from info from readarticleL
-int Hconnect(hlist &ascore, inlist &in, std::string name, std::string path); //connect all webs, returns scores
+int Hconnect(hlist &ascore, inlist &in, std::string name, std::string path, inlist &articles); //connect all webs, returns scores
+void HconnectNew(hlist &ascore, std::vector<std::string> &names, relist &redirs);
 
 //methods for parsing or searching
 void removeChars(std::string &str, char* charsToRemove); //remove all instances of characters
@@ -35,6 +40,15 @@ std::string finddir(std::string &name); //find the directory for the given file 
 bool badAType(std::string &name); //finds non-article pages
 
 int main(int argc, char* argv[]){
+    /*struct rlimit rl;
+    getrlimit(RLIMIT_NPROC,&rl);
+    printf("org limit is %lld\n", (long long int)rl.rlim_cur);
+    rl.rlim_cur = -1;
+    rl.rlim_max = -1;
+    setrlimit(RLIMIT_NPROC,&rl);
+    //sleep(1);
+    getrlimit(RLIMIT_NPROC,&rl);
+    printf("new limit is %lld\n", (long long int)rl.rlim_cur);*/
     std::ifstream read("articles.xml"); //XML file to be read
     if (!read.good()) { //testing for valid file
         std::cerr << "Can't open " << argv[1] << " to read.\n";
@@ -49,11 +63,12 @@ int main(int argc, char* argv[]){
     int skip = 0; //number of initial skip lines
     for(int x = 0;x<skip;x++) read>>word; //skipping X words, testing purposes only
 
-    std::cout<<"What would you like to do?\n1:Manual Walking\n2:Create info file system (Long Run Time)\n3:Analyze a dataset (Very Long Run Time)\n";
+    std::cout<<"What would you like to do?\n1:Manual Walking\n2:Create info file system (Long Run Time)\n3:Analyze a dataset (Very Long Run Time)\n4:Validate File System (Sorta Long Run Time)\n";
     std::cin>>option;
 
     if(option == 1){ //MANUAL WALKING
         //MANUAL WALKING: Testing purposes only really, but a default option for now.
+        std::ifstream read("OtherData/redirects.txt");
         while(next != 0){
             std::cout<<"Read (# of Words): "; //Words may not directly correlate to "words", but it's too insignificant to care about
             std::cin>>next; //next reads amount of words
@@ -93,13 +108,13 @@ int main(int argc, char* argv[]){
             if (mkdir("Articles", 0777) == -1){
                 std::cerr << "Error :  " << strerror(errno) << std::endl;  //Initializing Directory
             }else std::cout << "Article Directory created\n";
-            std::ofstream namelist("Articles/!!!articlenames.txt"); //a big'ol list of article names, just incase.
+            std::ofstream namelist("OtherData/!!!articlenames.txt"); //a big'ol list of article names, just incase.
             namelist<<"Article Names:\n";
         }else if(type == "W"){
             type = "Webs";
             if (mkdir("Webs", 0777) == -1) std::cerr << "Error:  " << strerror(errno) << std::endl; //Initializing Directory
             else std::cout << "Web Directory created\n";
-            std::ofstream namelist("Webs/!!!articlenames.txt"); //a big'ol list of article names, just incase.
+            std::ofstream namelist("OtherData/!!!articlenames.txt"); //a big'ol list of article names, just incase.
             namelist<<"Article Names:\n";
         }
         bool nart = false; //not article
@@ -131,14 +146,22 @@ int main(int argc, char* argv[]){
                     transform(name.begin(), name.end(), name.begin(), ::tolower); //to lowercase
                     if(!nart) red++; //article count for arbitrary amount
                     nart = badAType(name);
+                    if(!yred && !nart) writecount++;
                     pid_t pid = fork();
-                    if(pid == 0){
+                    if(pid == -1) return 0;
+                    if(pid > 0){
+                      int status;
+                      waitpid(-1,&status,WNOHANG);
+                    }
+                     if(pid == 0){
+                      //std::cout << "#STR# " << red << std::endl;
                       if(yred){
                         redirectadd(name, word, wredirect);
                       }else if(!nart){
                         if(type == "Webs") readarticleL(name,word,namelist,dirs); //read article is where each individual article is parsed
                         else if(type == "Articles") readarticle(name,word,namelist,dirs); //read article is where each individual article is parsed
                       }
+                      //std::cout << "|END| " << red << std::endl;
                       return 0;
                     }
 
@@ -149,7 +172,7 @@ int main(int argc, char* argv[]){
     }else if(option == 3){ //MAKING HLIST
         /**ANALYSIS BASED ON PREVIOUSLY GENERATED DATABASES**/
         ///at the moment its only text to webs
-        std::cout<<"Would you like to (W) Create Web link files (based on existing text files) (H) Calculate the Hitler Score?"<<std::endl;
+        std::cout<<"Would you like to\n(W) Create Web link files (based on existing text files)\n(H) Calculate the Hitler Score? (Faster, Less Accurate)\n(HN) Calculate the Hitler Score? (Slower, More Accurate)"<<std::endl;
         std::cin>>type;
         if(type == "W"){
             if (mkdir("Web", 0777) == -1) std::cerr << "Error :  " << strerror(errno) << std::endl;
@@ -175,31 +198,112 @@ int main(int argc, char* argv[]){
             closedir(dirp);
         }else if (type == "H"){
             type = "Webs";
-            std::ofstream scorelist("hitlerscores.txt"); //The file (will be huge and likely normally unreadable)
-            scorelist<<"ALL HITLER SCORES (unordered):\n";
+            std::ifstream file("OtherData/!!!articlenames.txt");
+            std::string name;
+            inlist names;
+            while(file >> name) names[name] = true;
             hlist articlescore;
             inlist in;
             inlist dirs;
             articlescore["adolf hitler"] = 0;
-            std::string direc = "Webs";
+            std::string direc = "./Webs";
             DIR* dirp = opendir(direc.c_str());
             struct dirent * dp;
+            readdir(dirp);
+            readdir(dirp);
+            int temp = 0;
             while ((dp = readdir(dirp)) != NULL) {
                 std::string loc = dp->d_name;
-                std::cout<<loc<<std::endl;
+                std::cout<<"1:"<<loc<<std::endl;
                 loc = "Webs/" + loc;
                 DIR* dirpdeep = opendir(loc.c_str());
                 struct dirent * dpdeep;
                 while ((dpdeep = readdir(dirpdeep)) != NULL) {
                     std::string filename = dpdeep->d_name;
-                    std::cout<<"     "<<filename<<" ";
-                    articlescore[filename] = Hconnect(articlescore, in, filename, loc);
+                    std::cout<<"2:"<<filename<<std::endl;
+                    articlescore[filename] = -1;
+                    articlescore[filename] = Hconnect(articlescore, in, filename, loc+"/"+filename, names);
+                    temp++;
+                    if(temp >= 3) exit(0);
+                    sleep(1);
+                    std::cout << filename << " distance: " << articlescore[filename] << std::endl;
                     in.clear();
                 }
                 closedir(dirpdeep);
             }
             closedir(dirp);
+            std::ofstream whscore("OtherData/hscores(unorganized).txt");
+            for(hlist::iterator itr = articlescore.begin();itr != articlescore.end();itr++){
+              whscore << itr->first << "::" << itr->second << std::endl;
+            }
+        }else if(type == "HN"){
+          hlist articlescore;
+          int testCount = 0;
+          std::vector<std::string> names;
+          std::string direc = "./Webs";
+          DIR* dirp = opendir(direc.c_str());
+          struct dirent * dp;
+          readdir(dirp);
+          readdir(dirp);
+          while ((dp = readdir(dirp)) != NULL) {
+              std::string loc = dp->d_name;
+              std::cout<<"Scanning in "<<loc<< "..."<<std::endl;
+              loc = "Webs/" + loc;
+              DIR* dirpdeep = opendir(loc.c_str());
+              struct dirent * dpdeep;
+              while ((dpdeep = readdir(dirpdeep)) != NULL) {
+                  std::string filename = dpdeep->d_name;
+                  if(filename != "." || filename != ".."){
+                    std::string nrmlname = filename.substr(0,filename.find(".txt"));
+                    names.push_back(nrmlname);
+                    //std::cout<<"2:"<<filename<<std::endl;
+                    testCount++;
+                    articlescore[nrmlname] = -1;
+                    if(testCount > 300000) goto skip;
+                  }
+              }
+              closedir(dirpdeep);
+          }
+          closedir(dirp);
+          skip:;
+          articlescore["adolf hitler"] = 0;
+          std::cout<<"Scanning in redirects..."<<std::endl;
+          relist redirects;
+          std::ifstream redir("OtherData/redirects.txt");
+          std::string rline,rstart,rend;
+          while(getline(redir,rline)){
+            rstart = rline.substr(0,rline.find("::"));
+            rend = rline.substr(rline.find("::")+2);
+            redirects[rstart] = rend;
+          }
+          std::cout<<"Starting Analysis!"<<std::endl;
+          type = "Webs";
+          HconnectNew(articlescore, names, redirects);
+
+
         }
+    }else if(option == 4){ //VALIDATE FILE SYSTEM
+      std::cout << "Beginning Validation...\n";
+      int fileCount = 0,fileCountInit;
+      std::string direc = "Webs";
+      DIR* dirp = opendir(direc.c_str());
+      struct dirent * dp;
+      while ((dp = readdir(dirp)) != NULL) {
+        //std::cout << "Opening Folder " << dp->d_name << std::endl;
+        fileCountInit = fileCount;
+        std::string loc = dp->d_name;
+        loc = "Webs/" + loc;
+        DIR* dirpdeep = opendir(loc.c_str());
+        struct dirent * dpdeep;
+        while ((dpdeep = readdir(dirpdeep)) != NULL){
+          fileCount++;
+          //std::cout << "File " << dpdeep->d_name << " found." << std::endl;
+        }
+        closedir(dirpdeep);
+        std::cout << "File Count in " << loc << ": " << fileCount-fileCountInit << " (" << fileCount << " overall)" << std::endl;
+      }
+      closedir(dirp);
+      std::cout << "Total File Count of Directory " << direc << ": " << fileCount;
     }else{
         std::cout<<"Unknown option selected. Program Quitting.";
     }
@@ -207,7 +311,6 @@ int main(int argc, char* argv[]){
 }
 
 void readarticle(std::string name, std::string &article,std::ofstream &namelist, inlist &dirs){
-    writecount++; //increment article count
     std::string dirname = finddir(name);
     if(!dirs[dirname]){
       if (mkdir(dirname.c_str(), 0777) != -1) std::cout<<"New Directory: "<<dirname<<std::endl; //making new directory if new index
@@ -227,7 +330,6 @@ void readarticle(std::string name, std::string &article,std::ofstream &namelist,
     articletxt.close(); //close it, then return
 }
 void readarticleL(std::string name, std::string &article,std::ofstream &namelist, inlist &dirs){
-    writecount++; //increment article count
     std::string dirname = finddir(name);
     if(!dirs[dirname]){
       if (mkdir(dirname.c_str(), 0777) != -1) std::cout << "New Directory: "<<dirname<<std::endl; //making new directory if new index
@@ -244,22 +346,20 @@ void readarticleL(std::string name, std::string &article,std::ofstream &namelist
     linkarticlesR(filename,dirname,article); ///pass the info to another function to find web
 }
 
-int Hconnect(hlist &ascore, inlist &in, std::string name, std::string path){
-    hlist::iterator itr = ascore.find(name);
+int Hconnect(hlist &ascore, inlist &in, std::string name, std::string path, inlist &articles){
+    int val = ascore[name];
     in[name] = true;
     inlist::iterator initr;
-    int top = 999999999;
-    if(itr == ascore.end()){ //if not in the hash map already
+    int top = -3;
+    if(val == -1){ //if not in the hash map already
+        top = 9999999;
+        std::cout << path << std::endl;
         std::ifstream file(path); //read in contents from parsed file
-
-        //TODO: CHANGE TO A HASH MAP FOR SPEED, DONT CHECK H INDIVIDUALLY
-
-
-
-
+        if(!file.good()) return -2;
         std::vector<std::string> links; //all the links in one particular file
         std::string link;
         while(file>>link){
+          //std::cout << link << std::endl;
             if(link == "adolf hitler"){
                 ascore[name] = 1;
                 return 1;
@@ -267,17 +367,86 @@ int Hconnect(hlist &ascore, inlist &in, std::string name, std::string path){
             links.push_back(link);
         } //by here we have a valid vector of links
         int distance;
-        std::string dir = finddir(name) + "/" + name; //get ready to recur
         for(unsigned int x = 0;x<links.size();x++){ //for all links in web file
             initr = in.find(links[x]); //checking if score already found
-            if(initr == in.end()) distance = Hconnect(ascore,in,links[x],dir)+1; //if not found, recur and find that link's hscore
-            else distance = ascore[links[x]] +1; //if found, use and add 1
-            if(distance < top) top = distance; //if smallest distance, use
+            if(initr == in.end()){
+              deep++;
+              std::cout << deep << ": ITERATE DEEPER" << std::endl;
+
+              std::string dir = finddir(links[x]) + "/" + links[x] + ".txt";
+              //sleep(1);
+              ascore[links[x]] = -1;
+              distance = Hconnect(ascore,in,links[x],dir,articles)+1; //if not found, recur and find that link's hscore
+              if(distance == 2) x = links.size()+2;
+
+              std::cout << deep << ": ITERATE END" << std::endl;
+              deep--;
+            }else distance = ascore[links[x]] +1; //if found, use and add 1
+            if(distance < top && distance >= 0) top = distance; //if smallest distance, use
         }
+        std::cout << "  " << name << " distance: " << distance << std::endl;
     }else{ //file has aleady been calculated
         return ascore[name];
     }
     return top;
+}
+
+void HconnectNew(hlist &ascore, std::vector<std::string> &names, relist &redirs){
+  std::unordered_set<std::string> list;
+  std::unordered_set<std::string> listtemp;
+  list.insert("adolf hitler"); //SEED ARTICLE
+
+  std::ofstream whscore("OtherData/hscores(organized).txt");
+
+  int score = 1;
+  startJump:;
+  int counter = 0;
+  std::cout<<"Finding Distances of " << score << std::endl;
+  bool remain = false;
+  for(unsigned int x = 0;x<names.size();x++){
+    if(x % 10000 == 0) std::cout << "(Dist " << score << ") Name index " << x << std::endl;
+    std::string filename = names[x];
+    if(ascore[filename] == -1){
+      remain = true;
+      std::string path = finddir(filename) + "/" + filename + ".txt";
+      //std::cout << filename << " is found in " << path << std::endl;
+      std::ifstream file(path); //read in contents from parsed file
+      if(file.good()){
+        std::string link;
+        while(getline(file,link)){
+          //if(link == "adolf hitler") std::cout << *list.find(link) << std::endl;
+          if(list.find(link) == list.end() && redirs.find(link) != redirs.end()){
+            if(link == "adolf hitler") std::cout << "redir at " << link << std::endl;
+            link = redirs.find(link)->second;
+          }
+          if(list.find(link) != list.end()){
+            //if(score == 1) std::cout << "Found one! Direct link to " << link << std::endl;
+            ascore[filename] = score;
+            listtemp.insert(filename);
+            counter++;
+            if(counter%100 == 0) std::cout<< counter << " files of distance " << score << " found so far. " << std::endl;
+            whscore << filename << "::" << score << std::endl;
+            names.erase(names.begin()+x);
+            x--;
+            goto nextFile;
+          }
+
+        }
+      }
+    }
+    nextFile:;
+  }
+  std::cout<< counter << " articles found with distance of " << score << "." << std::endl;
+  std::cout << names.size() << " articles remain untouched. " << std::endl;
+  score++;
+  list = listtemp;
+  listtemp.clear();
+  if(counter == 0) goto End;
+  if(remain) goto startJump;
+  End:;
+  for(hlist::iterator itr = ascore.begin();itr != ascore.end();itr++){
+    if(itr->second == -1) whscore << itr->first << "::" << itr->second << std::endl;
+  }
 }
 
 void linkarticles(std::string filename, std::string dirname, std::string directory){ //this one is for taking a text file system and turning it into webs
